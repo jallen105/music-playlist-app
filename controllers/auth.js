4436edd269dabe76ac526cbe72553eab7cc34de6
Jonathan <jonathanallen105@gmail.com>
2025-09-08 14:55:29 -0400

    setup complete

---
+++ controllers/auth.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcrypt')
+
+const User = require('../models/user.js')
+
+router.get('/sign-up', (req, res) => {
+    res.render('auth/sign-up.ejs')
+})
+
+router.get('/sign-in', (req, res) => {
+    res.render('../models/sign-in.ejs')
+})
+
+router.get('/sign-out', (req, res) => {
+    req.session.destroy()
+    res.redirect('/')
+})
+
+router.post('/sign-up', async (req, res) => {
+    try {
+        const userInDatabase = await User.findOne({ username: req.body.username })
+        if (userInDatabase) {
+            return res.send('Username already taken.')
+        } 
+        
+        if (req.body.password !== req.body.confirmPassword) {
+            return res.send('Passwords do not match.')
+        }
+
+        const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+        req.body.password = hashedPassword
+
+        await User.create(req.body)
+
+        res.redirect('/auth/sign-in')
+    } catch (err) {
+        console.log(err)
+        res.redirect('/')
+    }
+})
+
+router.post('/sign-in', async (req, res) => {
+    try {
+        const userInDatabase = await User.findOne({ username: req.body.username })
+        if (!userInDatabase) {
+            return res.send('Login failed. Please try again.')
+        }
+
+        const validPassword = bcrypt.compareSync(
+            req.body.password,
+            userInDatabase.password
+        )
+        if (!validPassword) {
+            return res.send('Login failed. Please try again.')
+        }
+
+        req.session.user = {
+            username: userInDatabase.username,
+            _id: userInDatabase._id
+        }
+
+        res.redirect('/')
+    } catch (err) {
+        console.log(err)
+        res.redirect('/')
+    }
+})
+
+module.exports = router
