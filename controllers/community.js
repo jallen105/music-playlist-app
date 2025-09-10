2c4cd58952f27c55d29c6bf05fa1bd54e3f037b9
Jonathan <jonathanallen105@gmail.com>
2025-09-10 13:47:58 -0400

    added favorite functionality

--- controllers/community.js
+++ controllers/community.js
@@ -15,9 +15,60 @@ router.get('/', async (req, res) => {
 router.get('/:playlistId', async (req, res) => {
     const currentPlaylist = await Playlist.findById(req.params.playlistId).populate('owner')
 
-    res.render('community/show.ejs', {
-        playlist: currentPlaylist
+    if (req.session.user) {
+
+        const userHasFavored = currentPlaylist.favoredBy.some((user) => user.equals(req.session.user._id))    
+        
+        res.render('community/show.ejs', {
+        playlist: currentPlaylist,
+        favored: userHasFavored,
     })
+
+    } else {
+        res.render('community/show.ejs', {
+        playlist: currentPlaylist,
+        favored: false,
+    })  
+    }
+
+
+
+})
+
+router.post('/:playlistId/favored-by/:userId', async (req, res) => {
+
+    try {
+
+        await Playlist.findByIdAndUpdate(req.params.playlistId, {
+            $push: { favoredBy: req.params.userId }
+        })
+
+        res.redirect(`/community/${req.params.playlistId}`)
+
+    } catch (err) {
+
+        console.log(err)
+        res.redirect('/')
+
+    }
+})
+
+router.delete('/:playlistId/favored-by/:userId', async (req, res) => {
+    
+    try {
+
+        await Playlist.findByIdAndUpdate(req.params.playlistId, {
+            $pull: { favoredBy: req.params.userId },
+        })
+
+        res.redirect(`/community/${req.params.playlistId}`)
+
+    } catch (err) {
+
+        console.log(err)
+        res.redirect('/')
+
+    }
 })
 
 module.exports = router
